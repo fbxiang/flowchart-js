a8580ceb308fdc0200603860470a658fdf2cd6dc
fbxiang <fxiang2@illinois.edu>
2017-06-22 20:46:52 -0500

    fix html

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 var path = require('path');
-var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./frontend/index.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
-    module: {
-        loaders: [
-            {test: /\.tsx?$/, loader: "ts-loader"},
-            {test: /\.css/, loader: "style-loader!css-loader"}
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 8080
-    }
+  entry: "./frontend/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./frontend/index.html"
+  })],
+  module: {
+    loaders: [
+      {test: /\.tsx?$/, loader: "ts-loader"},
+      {test: /\.css/, loader: "style-loader!css-loader"}
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080
+  }
 };
